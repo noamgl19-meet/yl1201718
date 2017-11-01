edf2c3b77dacf5690b1cf118d4537a0809ef850b
mohammad19-meet <mohammad19@meet.mit.edu>
2017-11-01 19:22:50 +0200

    i made a star!

---
+++ lab 3/TurtleShapes.py
@@ -0,0 +1,14 @@
+import turtle
+
+betweenangle = 90
+longside = 400
+shortside = 25
+triangle = 3
+angles = 70
+turtle.ht()
+def triangles():
+	for i in range(5):
+		turtle.forward(longside)
+		turtle.right(144)
+triangles()
+turtle.mainloop()
