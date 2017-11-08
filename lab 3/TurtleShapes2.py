2766afdea15886caab86fd06b14f60641ef5af1f
mohammad19-meet <mohammad19@meet.mit.edu>
2017-11-08 18:23:18 +0200

    lab3 is finished

---
+++ lab 3/TurtleShapes2.py
@@ -0,0 +1,14 @@
+import turtle
+turtle.ht()
+turtle.register_shape("arrow_l",((25,-25), (50,0), (50,50), (0,50), (0,0)))
+turtle.shape("arrow_l")
+turtle.fillcolor("green")
+turtle.left(90)
+turtle.st()
+turtle.goto(125,-1n25)
+turtle.goto(250,0)
+turtle.goto(250,250)
+turtle.goto(0,250)
+turtle.goto(0,0)
+
+turtle.mainloop()
---
+++ lab 3/lab3.py
@@ -0,0 +1,14 @@
+import turtle
+turtle.addshape("mario.gif")
+turtle.shape("mario.gif")
+turtle.home()
+i = 150
+a = 45
+g = 1
+while g < 100000000000000000:
+
+	turtle.forward(i)
+	turtle.right(a)
+	i = i 
+	a =  a 
+turtle.mainloop()
